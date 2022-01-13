a91f9e0e54796c27f94ff3bfb2b5172afe507f5c
Dete-Desai <detedesai@gmail.com>
2022-01-13 17:54:05 +0300

    Updates on mobile and backend application

--- food-eureka-back-end/src/main/kotlin/com/example/foodeurekabackend/FoodEurekaBackEndApplication.kt
+++ food-eureka-back-end/src/main/kotlin/com/example/foodeurekabackend/FoodEurekaBackEndApplication.kt
@@ -2,6 +2,9 @@ package com.example.foodeurekabackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class FoodEurekaBackEndApplication
@@ -9,3 +12,15 @@ class FoodEurekaBackEndApplication
 fun main(args: Array<String>) {
 	runApplication<FoodEurekaBackEndApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+	@GetMapping
+	fun index(): List<Message> = listOf(
+		Message("1", "Hello!"),
+		Message("2", "Welcome To Smart Restaurant"),
+		Message("3", "Variety Of Meals Available"),
+	)
+}
+
+data class Message(val id: String?, val text: String)
